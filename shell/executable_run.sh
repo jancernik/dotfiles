7a62a43ea5de77cc8034b128f95ab498d1832645
Jan Cernik <jancernik12@gmail.com>
2026-01-21 19:27:33 -0300

    Replace waybar with my own bar

--- shell/executable_run.sh
+++ shell/executable_run.sh
@@ -4,6 +4,8 @@ DIR="$(dirname "$(realpath "$0")")"
 
 cd "$DIR" && pnpm install
 
+ killall gjs
+
 if [[ "$1" == "--dev" ]]; then
 	trap 'pkill -f "gjs.*$DIR"; exit' INT TERM
 	find "$DIR/src" -name "*.tsx" -o -name "*.ts" -o -name "*.scss" | entr -r ags run "$DIR/src" --gtk 4
